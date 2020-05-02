b41492a2cce2af9d31982c9a4be6f7aaa08d6925
Tan Shi Yu <tanshiyu1999@gmail.com>
2020-05-02 11:08:06 +0800

    help

--- backend/Dijkstras_Algorithm_Math2.js
+++ backend/Dijkstras_Algorithm_Math2.js
@@ -28,35 +28,24 @@ function dijkstra(y1 = 2, x1 = 1, y2 = 2, x2 = 3) {
 
   var beginning = plane[y1][x1];
   var ending = plane[y2][x2];
-
   var found = [beginning];
-
   var founded = [];
-
   for (var int = 0; !(ending in found); int++ ) {
     var i = getIndexOfK(plane, found[int])[0];
     var j = getIndexOfK(plane, found[int])[1];
-
-
-    if (plane[i+ 1][j] != undefined) { //down
-      if (!(plane[i][j] in founded)) {
-        found.push(plane[i+1][j]);
-        console.log(plane[i+1][j]);
+    if (!(plane[i][j] in founded)) {
+      if (plane[i+1][j] != undefined) { //down
+          found.push(plane[i+1][j]);
+          console.log(plane[i+1][j]);
       }
-    }
-    if (plane[i-1][j] != undefined) { //up
-      if (!(plane[i][j] in founded)) {
-        found.push(plane[i-1][j]);
+      if (plane[i-1][j] != undefined) { //up
+          found.push(plane[i-1][j]);
       }
-    }
-    if (plane[i][j+1] != undefined) { //right
-      if (!(plane[i][j] in founded)) {
-        found.push(plane[i][j+1]);
+      if (plane[i][j+1] != undefined) { //right
+          found.push(plane[i][j+1]);
       }
-    }
-    if (plane[i][j-1] != undefined) { //left
-      if (!(plane[i][j] in founded)) {
-        found.push(plane[i][j-1]);
+      if (plane[i][j-1] != undefined) { //left
+          found.push(plane[i][j-1]);
       }
     }
     founded.push(plane[i][j])
