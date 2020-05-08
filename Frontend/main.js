04e969f94c908c40bb43e17a140d0c9a7626e6bd
Jason Lee <jasonepic0801@gmail.com>
2020-05-08 23:20:16 +0800

    Send help

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,6 +1,19 @@
-var test = new Vue ({
-  el: "#test",
+new Vue ({
+  el: "#app",
   data: {
-    box: "Hello Vue!"
+    message: "Keep clicking me to change me!"
+  },
+  methods: {
+    clickChange() {
+      if (this.message == "Keep clicking me to change me!") {
+        this.message = "Chocolate Pudding!";
+      } else if (this.message == "Chocolate Pudding!") {
+        this.message = "Vue is cool!";
+      } else if (this.message == "Vue is cool!") {
+        this.message = "I'm having a lot of fun!";
+      } else {
+        this.message = "Keep clicking me to change me!";
+      }
+    }
   }
-})
+});
